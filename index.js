258d1299153b497aaebda327798de17ad3f0797f
ayushkas3ra <raun4k@protonmail.com>
2026-02-04 08:33:58 +0530

    updated cors

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 mongoose
@@ -34,4 +40,6 @@ app.get("/api/institutes", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Server running on port ${PORT}"),
+);
